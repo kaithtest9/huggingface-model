f849e55e2752066d1ee59530b7b1e530a2f59368
salamer <salamer_gaga@163.com>
2024-09-16 21:40:43 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-pip install transformers flask gunicorn pillow requests
+pip install transformers[sentencepiece]
+pip install flask gunicorn pillow requests
 python load_model.py
