d3befbff10f873028f151650115912aba75c3ab8
salamer <salamer_gaga@163.com>
2024-09-16 21:35:05 +0800

    fix

---
+++ build.sh
@@ -0,0 +1,2 @@
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip install transformers flask gunicorn pillow requests
--- load_model.py
+++ load_model.py
@@ -1,14 +1,19 @@
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
 import requests
+import time
 
 # load image from the IAM database
 with open("20150624_135325.jpg", "rb") as f:
     image = Image.open(f).convert("RGB")
 
-processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-handwritten')
-model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-handwritten')
+start = time.time()
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-small-handwritten')
+model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-small-handwritten')
 pixel_values = processor(images=image, return_tensors="pt").pixel_values
 
 generated_ids = model.generate(pixel_values)
 generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print("time taken: ", time.time() - start)
+
+print("text generated: ", generated_text)
